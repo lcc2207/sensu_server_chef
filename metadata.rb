5f8497e8f11ae38f6f7c75ac0112394bb04bc0a5
lcc2207 <lynn@fidmail.com>
2018-04-27 12:51:20 -0500

    update sensu cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lynn@scalr.com'
 license 'All Rights Reserved'
 description 'Installs/Configures sensu_server_chef'
 long_description 'Installs/Configures sensu_server_chef'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 %w(centos redhat ubuntu).each do |os|
@@ -14,4 +14,4 @@ end
 issues_url 'https://github.ge.com/lcc2207/sensu_server_chef'
 source_url 'https://github.ge.com/lcc2207/sensu_server_chef'
 
-depends 'sensu', '~> 4.0.6'
+depends 'sensu', '~> 4.3.1'
